f64118eafc514f5ad6712d378e880c14b162c191
BJ Hansen <bjhansen@benjaminhansen.net>
2022-10-03 21:22:45 -0500

    Fixing scripts and styles URLs

--- scripts/twc3.js
+++ scripts/twc3.js
@@ -11574,11 +11574,11 @@ var ShowDopplerMap = function (WeatherParameters)
     };
     if (WeatherParameters.State == "HI")
     {
-        img.src = "images/HawaiiRadarMap2.png";
+        img.src = "images/hawaiiradarmap2.png";
     }
     else
     {
-        img.src = "images/4000RadarMap2.jpg";
+        img.src = "images/4000radarmap2.jpg";
         //img.src = "images/4000RadarMap2.png";
     }
 }
